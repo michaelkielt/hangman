80be11435847b0b0ec9931effd08d3d5e67be126
michaelkielt <michaelkielt@gmail.com>
2023-10-03 18:57:32 +0100

    Adds while loop to ask user for guess

---
+++ hangman/milestone-3.py
@@ -0,0 +1,7 @@
+# Creating a while loop that will ask the user for guesses and validate them
+# Setting the condition to 'True' so that the loop will continuously run
+
+while (True):
+    guess = input("Please guess by entering a letter")
+
+    
