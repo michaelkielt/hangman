38fda4f227f3edbc19358987e2bc3b84374955ba
michaelkielt <michaelkielt@gmail.com>
2023-10-03 19:23:08 +0100

    adds if-statement to check if guess is in word

--- hangman/milestone-3.py
+++ hangman/milestone-3.py
@@ -10,3 +10,11 @@ while (True):
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+secret_word = "apple"
+
+# Checking if the guessed letter is in the secret word
+if guess in secret_word:
+    print("Good guess! '{guess}' is in the word.")
+else:
+    print("Sorry, '{guess}' is not in the word. Try again.")    
