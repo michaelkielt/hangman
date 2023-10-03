0e431075bfb91514618416f94650c07707c8ad8c
michaelkielt <michaelkielt@gmail.com>
2023-10-03 19:02:40 +0100

    Adds if statement to validate the user's guess.

--- hangman/milestone-3.py
+++ hangman/milestone-3.py
@@ -4,4 +4,9 @@
 while (True):
     guess = input("Please guess by entering a letter")
 
-    
+    # Check if the guess is a single letter and alphabetical
+    # If the guess is valid the loop breaks
+    if len(guess) == 1 and guess.isalpha():
+        break
+    else:
+        print("Invalid letter. Please, enter a single alphabetical character.")
