b4a22da361b868eb2049c622d91fc6291cd44829
michaelkielt <michaelkielt@gmail.com>
2023-10-03 19:38:21 +0100

    adds nested if statement to check guess is valid

--- hangman/milestone-3.py
+++ hangman/milestone-3.py
@@ -1,20 +1,23 @@
 # Creating a while loop that will ask the user for guesses and validate them
 # Setting the condition to 'True' so that the loop will continuously run
 
+secret_word = "apple"
+
 while (True):
     guess = input("Please guess by entering a letter")
 
     # Check if the guess is a single letter and alphabetical
     # If the guess is valid the loop breaks
     if len(guess) == 1 and guess.isalpha():
-        break
+        # Checking if the guessed letter is in the secret word
+        if guess in secret_word:
+            print(f"Good guess! '{guess}' is in the word.")    
+            break
+        else:
+            print(f"Sorry, '{guess}' is not in the word. Try again.")
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
 
-secret_word = "apple"
 
-# Checking if the guessed letter is in the secret word
-if guess in secret_word:
-    print("Good guess! '{guess}' is in the word.")
-else:
-    print("Sorry, '{guess}' is not in the word. Try again.")    
+       
+    
