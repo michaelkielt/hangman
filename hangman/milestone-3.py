2c26d22741172d4ea40af1216b980cea39fd0020
michaelkielt <michaelkielt@gmail.com>
2023-10-03 20:58:29 +0100

    Adds 'check_guess' and 'ask_for_input' funcs

--- hangman/milestone-3.py
+++ hangman/milestone-3.py
@@ -3,21 +3,38 @@
 
 secret_word = "apple"
 
-while (True):
-    guess = input("Please guess by entering a letter")
+#while (True):
+#    guess = input("Please guess by entering a letter")
 
     # Check if the guess is a single letter and alphabetical
     # If the guess is valid the loop breaks
-    if len(guess) == 1 and guess.isalpha():
+#    if len(guess) == 1 and guess.isalpha():
         # Checking if the guessed letter is in the secret word
+#        if guess in secret_word:
+#            print(f"Good guess! '{guess}' is in the word.")    
+#            break
+#        else:
+#            print(f"Sorry, '{guess}' is not in the word. Try again.")
+#    else:
+#        print("Invalid letter. Please, enter a single alphabetical character.")
+
+# Creating a function for the guess checking logic
+def check_guess(guess):
+        guess = guess.lower()
         if guess in secret_word:
             print(f"Good guess! '{guess}' is in the word.")    
+        else:
+            print(f"Sorry, '{guess}' is not in the word. Try again.") 
+
+
+def ask_for_input():
+    while (True):
+        guess = input("Please guess by entering a letter")
+        if len(guess) == 1 and guess.isalpha():
+            check_guess(guess)
             break
         else:
-            print(f"Sorry, '{guess}' is not in the word. Try again.")
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+            print("Invalid letter. Please, enter a single alphabetical character.")
 
 
-       
     
